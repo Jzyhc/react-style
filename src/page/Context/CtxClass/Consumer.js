5eff63e619d1ce775b339d7a6eb49229f62d6176
jincheng <j15688838318@163.com>
2021-05-12 21:07:57 +0800

    add.

--- src/page/Context/CtxClass/Consumer.js
+++ src/page/Context/CtxClass/Consumer.js
@@ -1,12 +1,14 @@
 /*
  * @Author       : jincheng
  * @Date         : 2021-05-12 17:59:26
- * @LastEditTime : 2021-05-12 18:06:50
+ * @LastEditTime : 2021-05-12 21:07:44
  * @LastEditors  : jincheng
  * @FilePath     : /react-style/src/page/context/CtxClass/Consumer.js
  */
 import React from 'react'
 import { ThemeConsumer } from './themeContext'
+
+// 函数组件获取context中的数据，使用Consumer 进行包裹; Consumer 会返一个函数组件
 export default function Consumer(props) {
   return (
     <div>
